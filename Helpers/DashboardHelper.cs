32d3e50777fbea6184be03fe3f48e066d6adf4e1
salmasgh <selma.sghaier@insat.ucar.tn>
2025-02-01 11:03:37 +0100

    updating dashboard structure

---
+++ Helpers/DashboardHelper.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _.Models;
+
+// This helper is generic and can be used in any controller
+// Functions should be generic (can be used in any controller)
+// NO APPLICATIONDBCONTEXT PASSED IN ARGUMENT !!!!!
+// pls
+
+namespace _.Helpers
+{
+    public static class DashboardHelper
+    {
+        
+    }
+}
